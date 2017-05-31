e42592768602914183a24f65c0b22dedb975219d
Maurits Evers <maurits.evers@anu.edu.au>
2017-05-31 11:42:16 +1000

    Pairwise plotting is now separate function in myfunctions.R; added HEATR3 label

---
+++ myfunctions.R
@@ -0,0 +1,38 @@
+ggpairwise <- function(df, xlab = "", ylab = "", title = "") {
+  # Pairwise plots
+  my_fn <- function(data, mapping, method = "lm", ...) {
+    #print(str(data));
+    xVal <- data[, as.character(mapping$x)];
+    yVal <- data[, as.character(mapping$y)];
+    lbl <- data[, as.character(mapping$label)];
+    df.highlight <- cbind.data.frame(x = xVal, y = yVal, id = lbl);
+    df.highlight <- df.highlight[grep("HEATR3", df.highlight$id), ];
+    df.highlight$id <- "HEATR3";
+    #print(df.highlight);
+    label.Ntot <- sprintf("N=%i", nrow(data[complete.cases(xVal, yVal), ]));
+    p <- ggplot(data = data, mapping = mapping);
+    p <- p + geom_point();
+    p <- p + geom_point(data = df.highlight, aes(x = x, y = y), size = 4, fill = "red", colour = "red", alpha = 0.5);
+    p <- p + geom_text_repel(data = df.highlight, aes(x = x, y = y, label = id), colour = "red", size = 3);
+    p <- p + geom_abline(slope = 1, intercept = 0, colour = "red", linetype = 3);
+    p <- p + geom_smooth(method = method, fullrange = TRUE, ...);
+    p <- p + geom_text(aes(x = -Inf, y = Inf, hjust = 0, vjust = 1, label = label.Ntot));
+    return(p);
+  }
+  gg <- ggpairs(df, mapping = aes(label = id), columns = 1:(ncol(df)-1), lower = list(continuous = my_fn));
+  gg <- gg + theme_bw();
+  gg <- gg + theme(
+    strip.background = element_blank(),
+    panel.border = element_rect(colour = "black"),
+    legend.position = "bottom",
+    legend.key = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank()
+  );
+  gg <- gg + labs(
+    x = xlab,
+    y = ylab,
+    title = title
+  );
+  gg;
+}
